a4efcbdeda7941bc9c8781301e94007d8b0d37f7
izabelleleticia <izabelle.lcerqueira@gmail.com>
2024-12-13 11:34:34 -0300

    alteracaoemsala

--- conteudo/blog.php
+++ conteudo/blog.php
@@ -41,11 +41,18 @@
                       <img src="img/conrad.png" alt="">
                       <img src="img/jeremiah.png" alt="">
                     </div>
+        <div class="sobreLivros">
+            <h1>Resenha</h1>
+            <h2>Trilogia Verão</h2>
+            <p>Com uma história encantadora e personagens inesquecíveis, a Trilogia Verão acompanha os amores, as desilusões e os dilemas da protagonista Belly dos 15 aos 24 anos.
+                 Em meio a descobertas e mudanças, ela se apaixona, se envolve em um triângulo amoroso, entra na universidade e descobre que amadurecer também significa tomar decisões difíceis.
+                  A trama escrita por Jenny Han, autora de Para todos os garotos que já amei , inspirou a série de sucesso O verão que mudou minha vida , do Amazon Prime Video.
+
+A vida de Isabel Conklin é marcada pelas férias de verão. As outras estações do ano são como um intervalo, dias que passam lentamente enquanto ela espera que o sol lhe traga de volta o que mais ama:
+ o mar, descanso, diversão e, principalmente, Conrad e Jeremiah Fisher. Os garotos da família Fisher sempre estiveram ao lado de Belly em suas aventuras.
+  Conrad é ousado, sombrio, inteligente. Já Jeremiah é confiável, engraçado, espontâneo. Mesmo sendo tão diferentes, os três constroem uma amizade que parece inabalável. Apenas parece...</p>
+        </div>
                     
         </div>
-
-                    <div>
-                        <img src="serie.jpg" alt="">
-                    </div>
                     </article>
         </section>
--- conteudo/rodape.php
+++ conteudo/rodape.php
@@ -2,7 +2,26 @@
             <article class="site">
                 <img src="img/estreladomar.png" alt="estreladomar">
                 <img src="img/concha.png" alt="concha">
-                    
+            <div class=textoRodape>
+                <!-- <p>
+                    A BooksAndFun é uma livraria que pensa no leitor e seus gostos.
+                </p> -->
+            <nav class="sobre">
+                <h2>Sobre nós</h2>
+                <ul>
+                    <a href="sobre.php">
+                        <li>Sobre</li>
+                    </a>
+                    <a href="contato.php">
+                        <li>Contato</li>
+                    </a>
+                </ul>
+                <ul>
+                    <a href="blog.php">
+                        <li>Blog</li>
+                    </a>
+            </nav>
+            </div>        
                
             </article>
             <p>Copyright © 2024 - Todos os diretitos reservados | SENAC SMP - TI26</p>
